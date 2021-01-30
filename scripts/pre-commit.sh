538e7a772e530d52e4f16553cc82769c0f57170c
Aaquib Equbal <armanaaquibequbal@gmail.com>
2021-01-30 15:03:20 +0530

    Aaquib #14 Update pre-commit git hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,12 +2,12 @@
 
 cd $(git rev-parse --show-toplevel) || exit 1
 
-if [ $(./run backend_test) -ne 0 ]; then
-  echo "backend tests are failing"
+if ! ./run frontend_test; then
+  echo "frontend tests are failing"
   exit 1
 fi
 
-if [ $(./run frontend_test) -ne 0 ]; then
-  echo "frontend tests are failing"
+if ! ./run backend_test; then
+  echo "backend tests are failing"
   exit 1
 fi
